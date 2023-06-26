dff9adb694812a0474e9d480ff5888dea1ee82d1
LSanchez08 <luis.alberto.sanchez.rios@live.com.mx>
2023-06-26 15:17:38 -0600

    node creation

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const { GoogleAuth } = require('google-auth-library');
+const { google } = require('googleapis');
+
+const app = express();
+
+app.get('/', async (req, res) => {
+  const spreadsheetId = '12r7yInB2F-72cmEFHdCYoZx9QHFmkl0CVJynfoUmqjc';
+  const range = 'Sheet1!A1:B6';
+  const auth = new GoogleAuth({
+    keyFile: 'credentials.json',
+    scope: 'https://www.googleapis.com/auth/spreadsheets.readonly'
+  });
+  const service = google.sheets({version: 'v4', auth});
+
+  try {
+    const result = await service.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    });
+    const numRows = result.data.values ? result.data.values.length : 0;
+    console.log(`${numRows} rows retrieved.`);
+
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+
+    res.json('Error');
+  }
+});
+
+app.listen(1337, (req, res) => console.log('running on 1337'));
